37a19ac8482cb2e7efc759f45e6a0f61ae0ad519
Ankit <76108902+ankitXD@users.noreply.github.com>
2024-07-14 20:01:38 +0530

    Add files via upload

---
+++ Day-1.js
@@ -0,0 +1,49 @@
+//Activity 1
+
+//Task 1
+var a = 80
+console.log(a) //80
+
+//Task 2
+let b = 'Hello'
+console.log(b) //Hello
+
+//Activity 2
+
+//Task 3
+const c = 'True'
+console.log(c) //True
+
+//Activity 3
+
+//Task 4
+let a1 = 357834
+let b1 = 'Ankit'
+let c1 = true
+let d1 = {
+    'Name' : 'Ankit',
+    'Branch' : 'CSE',
+}
+let e1 = [12,'name',53,12,4.6]
+
+console.log(typeof(a1))//number
+console.log(typeof(b1))//string
+console.log(typeof(c1))//boolean
+console.log(typeof(d1))//object
+console.log(typeof(e1))//object
+
+//Activity 4
+
+//Task 5
+let val = 46
+console.log(val)//46
+val = 43
+console.log(val)//43
+
+//Activity 5
+
+//Task 6
+const variable_1 = 74
+console.log(variable_1)//74
+variable_1 = 73  
+console.log(variable_1) // TypeError: Assignment to constant variable.
